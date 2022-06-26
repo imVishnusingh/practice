b4cf90862fd49086065c71610f348efebaf83c00
Vishnu Kumar <108206755+imVishnusingh@users.noreply.github.com>
2022-06-26 11:49:31 +0530

    Add files via upload

---
+++ driving.py
@@ -0,0 +1,9 @@
+age = int(input("Enter Your Age: "))
+if age<18:
+    print("Oops, You Are Not Allowed!")
+if age>18:
+    print("Congrats You Are Allowed To Drive")
+if age==18:
+    print("We Cant decide you eligibility")
+if age>75:
+    print("Dont Risk Your Life! Hire a Driver")
